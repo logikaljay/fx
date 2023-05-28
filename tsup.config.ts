bcfc1ffa073bf7392bf58a7de1177e946ca123cf
Jay Baker <logikal@gmail.com>
2023-05-28 22:54:37 +1200

    fix: dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup'
 import { spawn } from 'child_process'
 
 export default defineConfig({
+  external: ['esbuild', 'tsup']
   
   // async onSuccess() {
   //   let proc = spawn("node", ['./dist/cli.js'], {
